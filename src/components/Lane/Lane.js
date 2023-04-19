1d60d2f98b21b9660ea68670566e0ef0e0bd6881
Eddy-123 <adegnandjoueddy12@gmail.com>
2023-04-19 03:54:05 +0100

    drag and drop on tasks

--- src/components/Lane/Lane.js
+++ src/components/Lane/Lane.js
@@ -1,10 +1,33 @@
+import styled from 'styled-components';
 import Task from '../Task/Task';
-import './Lane.css';
 
-function Lane({ title, loading, error, tasks }) {
+const LaneWrapper = styled.div`
+  text-align: left;
+  padding: 0;
+  background: lightGray;
+  border-radius: 20px;
+  min-height: 50vh;
+  width: 20vw;
+
+  @media (max-width: 768px) {
+    margin-bottom: 5%;
+  }
+`;
+
+const Title = styled.h2`
+  width: 100%;
+  padding-bottom: 10px;
+  text-align: center;
+  border-bottom: 1px solid darkGray;
+`;
+
+function Lane({ laneId, title, loading, error, tasks, onDragStart, onDragOver, onDrop }) {
   return (
-    <div className='Lane-wrapper'>
-      <h2>{title}</h2>
+    <LaneWrapper
+      onDragOver={onDragOver}
+      onDrop={(e) => onDrop(e, laneId)}
+    >
+      <Title>{title}</Title>
       {loading || error ? (
         <span>{ error || 'Loading...'}</span>
       ) : (
@@ -14,10 +37,11 @@ function Lane({ title, loading, error, tasks }) {
             id={task.id}
             title={task.title}
             body={task.body}
+            onDragStart={onDragStart}
           />
         ))
       )}
-    </div>
+    </LaneWrapper>
   );
 }
 
